ea792851379977206f5a62803a27da2401c3e0ee
sammyne <lixiangmin01@baidu.com>
2019-12-16 10:01:27 +0800

    docs(hash): basic description of the 'Hash' trait

--- src/hash.rs
+++ src/hash.rs
@@ -1,12 +1,22 @@
-pub use digest::Digest;
 use std::io::Write;
 
+/// The `Hash` trait specifies the common interface for hash functions.
+///
+/// The `write` from `Write` trait adds more data to the running hash.
+/// It never returns an error.
 pub trait Hash: Write {
+    /// block_size returns the hash's underlying block size.
+    /// The Write method must be able to accept any amount
+    /// of data, but it may operate more efficiently if all writes
+    /// are a multiple of the block size.
     fn block_size() -> usize;
+    /// new creates a fresh hasher instance
     fn new() -> Self;
+    /// size returns the number of bytes Sum will return
     fn size() -> usize;
 
+    /// reset resets the Hash to its initial state
     fn reset(&mut self);
+    /// sum returns the resulting slice. It does not change the underlying hash state.
     fn sum(&self) -> Vec<u8>;
-    //fn write(&mut self, buf: &[u8]) -> Result<usize, String>;
 }
