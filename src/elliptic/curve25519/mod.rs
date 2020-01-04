e336ae7952538ced22516aa9588d8cdd5125d1b0
sammyne <lixiangmin01@baidu.com>
2020-01-04 16:43:47 +0800

    docs(elliptic/curve25519): add docs

--- src/elliptic/curve25519/mod.rs
+++ src/elliptic/curve25519/mod.rs
@@ -1,7 +1,18 @@
+//! module curve25519 provides an implementation of the X25519 function, which performs scalar
+//! multiplication on the elliptic curve known as Curve25519. See [RFC 7748][1].
+//!
+//! [1]: http://tools.ietf.org/html/rfc7748
+
+/// BASEPOINT is the canonical Curve25519 generator
 pub const BASEPOINT: [u8; 32] = x25519_dalek::X25519_BASEPOINT_BYTES;
 
+/// x25519 is an alias of [scalar_mult()](fn.scalar_mult.html).
 pub use x25519_dalek::x25519;
 
+/// scalar_mult calculates the product scalar * BASEPOINT.
+pub use x25519_dalek::x25519 as scalar_mult;
+
+/// scalar_base_mult calculates the product k * BASEPOINT, where k is a scalar
 pub fn scalar_base_mult(k: [u8; 32]) -> [u8; 32] {
     x25519(k, BASEPOINT)
 }
--- src/elliptic/mod.rs
+++ src/elliptic/mod.rs
@@ -1,1 +1,4 @@
+//! module elliptic implements several standard elliptic curves over prime fields
+//!
+
 pub mod curve25519;
