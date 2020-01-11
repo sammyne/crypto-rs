f62ed94ea2a163eeb84168a29c65fc684367159c
sammyne <lixiangmin01@baidu.com>
2020-01-11 10:44:42 +0800

    docs(md5): document the code

--- src/md5/mod.rs
+++ src/md5/mod.rs
@@ -1,15 +1,23 @@
+//! module md5 implements the MD5 hash algorithm as defined in RFC 1321.
+//!
+//! MD5 is cryptographically broken and should not be used for secure applications.
+
 use std::io::{self, Write};
 
 use md5::{Digest, Md5};
 
 pub use super::Hash;
 
+/// A MD5 is an instance of MD5.
 pub struct MD5(Md5);
 
+/// The blocksize of MD5 in bytes.
 pub const BLOCK_SIZE: usize = 64;
+/// The size of an MD5 checksum in bytes.
 pub const SIZE: usize = 16;
 
 impl Hash for MD5 {
+    /// New returns a new hash.Hash computing the MD5 checksum.
     fn new() -> Self {
         Self(Md5::new())
     }
@@ -47,8 +55,9 @@ impl Write for MD5 {
     }
 }
 
-pub fn sum(b: &[u8]) -> [u8; SIZE] {
-    let d = Md5::digest(b);
+/// sum returns the MD5 checksum of the data.
+pub fn sum(data: &[u8]) -> [u8; SIZE] {
+    let d = Md5::digest(data);
 
     let mut out = [0u8; SIZE];
     (&mut out[..]).copy_from_slice(d.as_slice());
